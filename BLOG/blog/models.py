82614ef0c7c0e622367225b208c8302927fe4072
mechanic <ornobtanvir.git@gmail.com>
2020-01-15 10:45:15 +0600

    added

--- BLOG/blog/models.py
+++ BLOG/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+## create the author model
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    ## this is the decorator function
+
+    def __str__(self):
+        return self.name 
+
+
+## this is Article Model
+
+class Article(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    body = models.TextField()
+
+    ## this is the foreign key
+    author = models.ForeignKey('Author',related_name='articles',on_delete=models.CASCADE)
+
+    ## this foreign key connect the author field to the author Table
+    # and if the author got deleted the post of all of its will be 
+    # deleted too
+
+    def __str__(self):
+        return self.author 
+
+
